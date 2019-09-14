bbb5b4261d8b64693db607ae9ab752a60cc91b44
davidlinke <davidlinke@davidlinke.com>
2019-09-14 16:17:17 -0400

    added more characters to ignore in description

--- app/graphql/mutations/add_podcast.rb
+++ app/graphql/mutations/add_podcast.rb
@@ -12,31 +12,47 @@ class Mutations::AddPodcast < Mutations::BaseMutation
   
     def resolve(rss_url:, premium_podcast:, added_by_user:, ignore_keywords:)
         open(rss_url) do |rss|
-            feed = RSS::Parser.parse(rss)
+            begin
+                feed = RSS::Parser.parse(rss)
 
-            cleaned_description = feed.channel.description.gsub("\n", "").gsub("<p>", "").gsub("</p>", "").strip
-    
-            podcast = Podcast.new(
-                title: feed.channel.title,
-                rss_url: rss_url,
-                web_url: feed.channel.link,
-                description: cleaned_description,
-                image_url: feed.channel.image.url,
-                added_by_user: added_by_user,
-                premium_podcast: premium_podcast,
-                ignore_keywords: ignore_keywords
-            )
-                
-            if podcast.save
-                {
-                    podcast: podcast,
-                    errors: [],
-                }
-            else
-                {
-                    podcast: nil,
-                    errors: podcast.errors.full_messages
-                }
+                cleaned_description = feed.channel.description.gsub("\n", "").gsub("<p>", "").gsub("</p>", "").gsub("&nbsp;", "").gsub("<em>", "").gsub("</em>", "").strip
+
+                # puts 'TITLE'
+                # puts feed.channel.title
+                # puts 'RSS URL'
+                # puts rss_url
+                # puts 'WEB URL'
+                # puts feed.channel.link
+                # puts 'DESCRIPTION'
+                # puts cleaned_description
+                # puts 'IMAGE URL'
+                # puts feed.channel.image.url
+        
+                podcast = Podcast.new(
+                    title: feed.channel.title,
+                    rss_url: rss_url,
+                    web_url: feed.channel.link,
+                    description: cleaned_description,
+                    image_url: feed.channel.image.url,
+                    added_by_user: added_by_user,
+                    premium_podcast: premium_podcast,
+                    ignore_keywords: ignore_keywords
+                )
+                    
+                if podcast.save
+                    {
+                        podcast: podcast,
+                        errors: [],
+                    }
+                else
+                    {
+                        podcast: nil,
+                        errors: podcast.errors.full_messages
+                    }
+                end
+
+            # rescue RSS::Error
+            #     puts 'Bad RSS feed'
             end
         end
     end
