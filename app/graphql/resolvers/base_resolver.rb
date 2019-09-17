e2b9d6357341c1a6e31e859f3fae16d8ce0eb7cb
davidlinke <davidlinke@davidlinke.com>
2019-09-17 10:48:37 -0400

    removed resolvers for debugging

--- app/graphql/resolvers/base_resolver.rb
+++
@@ -1,9 +0,0 @@
-module Resolvers
-    class BaseResolver
-      include SearchObject.module(:GraphQL)
-  
-      def escape_search_term(term)
-        "%#{term.gsub(/\s+/, '%')}%"
-      end
-    end
-  end
